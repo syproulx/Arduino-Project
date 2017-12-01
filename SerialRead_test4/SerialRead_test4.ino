bb26ad9cf4bc438971986de53122f690b81e73e1
syproulx <syproulx575@gmail.com>
2017-12-01 15:36:53 -0500

    Read User Input
    Avec case pour faire un menu

---
+++ SerialRead_test4/SerialRead_test4.ino
@@ -0,0 +1,43 @@
+//Test l envoie de charactere sur le port serie vers le PC
+//
+
+char receivedChar;
+boolean newData = false;
+
+void setup() {
+ Serial.begin(9600);
+ Serial.println("<Arduino is ready>");
+}
+
+void loop() {
+ recvOneChar();
+ switch (receivedChar) {
+   case '1':
+     showNewData();
+     break;
+   case '2':
+     showNewData();
+     break;
+ }
+// showNewData();
+}
+
+void recvOneChar() {
+ if (Serial.available() > 0) {
+   receivedChar = Serial.read();
+   newData = true;
+ }
+}
+//test pour git
+void showNewData() {
+ if (newData == true) {
+   //Pour pouvoir afficher dans le moniteur serie Arduino un string avec une variable sur la 
+   //meme commande il faut utilsier la sybtaxe suivante
+   Serial.println(String("This just in ... ") + receivedChar);
+   //Serial.println(receivedChar);
+   newData = false;
+ }
+}
+
+
+
